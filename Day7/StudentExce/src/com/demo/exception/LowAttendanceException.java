326990745320d953311b00dce7f9119952d8c473
SnehalSYadav <snehalyadav2003@gmail.com>
2025-10-25 21:35:21 +0530

    Day7

---
+++ Day7/StudentExce/src/com/demo/exception/LowAttendanceException.java
@@ -0,0 +1,12 @@
+
+package com.demo.exception;
+
+public class LowAttendanceException extends Exception {
+
+	public LowAttendanceException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
